0d78f932be6980357f3340063888e6042a07cd71
MaSsoumeh <massoumehzarei@gmail.com>
2021-12-07 01:29:08 +0330

    add state

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,10 +1,14 @@
 import Star from './Star';
+import { useState } from 'react';
+
 const createArray = (length) => [...Array(length)];
 const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(2);
+
   return (
     <>
       {createArray(totalStars).map((n, i) => {
-        return <Star key={i} />;
+        return <Star key={i} selected={selectedStars > i} />;
       })}
     </>
   );
